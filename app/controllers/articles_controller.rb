182313443cc08fdc16597b9465fd81949460e135
rails Udemy Course <vachiravit.va@gmail.com>
2017-07-03 12:19:22 +0700

    prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @article = Article.all
   end
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
 	def create
@@ -23,7 +23,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -35,12 +34,10 @@ class ArticlesController < ApplicationController
 
   def show
 
-		@article = Article.find(params[:id])
 	end
 
   def destroy
 
-    @article = Article.find(params[:id])
 
     @article.destroy
 
@@ -52,6 +49,10 @@ class ArticlesController < ApplicationController
 
 
 	private
+  def  set_article
+    @article = Article.find(params[:id])
+
+  end
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
