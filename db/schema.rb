c385eab10cca7cd0e008ba8cb9540901a2853f3f
rails Udemy Course <vachiravit.va@gmail.com>
2017-07-30 01:07:33 +0700

    add secure pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628094356) do
+ActiveRecord::Schema.define(version: 20170729180140) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "create_at"
     t.datetime "update_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
